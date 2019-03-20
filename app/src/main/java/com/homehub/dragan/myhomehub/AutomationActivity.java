cd97b48e30cf0753948aae5d27442b0d014d9436
Jack Webster <websjack@sheridancollege.ca>
2019-03-20 14:10:20 -0400

    Got rid of Device Control activity, merged all its functionality into Automation Activity (where it was supposed to be in the first place)

--- app/src/main/java/com/homehub/dragan/myhomehub/AutomationActivity.java
+++ app/src/main/java/com/homehub/dragan/myhomehub/AutomationActivity.java
@@ -2,37 +2,28 @@ package com.homehub.dragan.myhomehub;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
-import android.widget.TextView;
 
 public class AutomationActivity extends AppCompatActivity {
-
-    private TextView mTextMessage;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
         @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        public boolean onNavigationItemSelected(MenuItem item) {
             Intent intent;
             switch (item.getItemId()) {
                 case R.id.navigation_dashboard:
-
-                    //intent = new Intent(MainActivity.this, MainActivity.class);
-                    //startActivity(intent);
                     AutomationActivity.super.finish();
                     return true;
                 case R.id.navigation_account:
-
                     intent = new Intent(AutomationActivity.this, AccountActivity.class);
                     //intent.putExtra("channel", "4lmrrOD8Ll2SkO2A");
                     startActivity(intent);
                     return true;
                 case R.id.navigation_automation:
-
                     return true;
             }
             return false;
@@ -42,7 +33,9 @@ public class AutomationActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_device_control);
-    }
+        setContentView(R.layout.activity_automation);
 
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
 }
