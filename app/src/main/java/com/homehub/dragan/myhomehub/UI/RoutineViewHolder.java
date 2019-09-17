415d246688efc9d73693125b1abb983faa432bc5
Jack Webster <websjack@sheridancollege.ca>
2019-09-17 15:02:44 -0400

    Added stuff to implement the RecyclerView for the list of routines (it's not fully done yet though, I'm just committing it to be safe)

---
+++ app/src/main/java/com/homehub/dragan/myhomehub/UI/RoutineViewHolder.java
@@ -0,0 +1,13 @@
+package com.homehub.dragan.myhomehub.UI;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+public class RoutineViewHolder extends RecyclerView.ViewHolder {
+
+    public RoutineViewHolder(View v) {
+        super(v);
+
+
+    }
+}
